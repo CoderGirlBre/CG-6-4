ece5bb4c824c38fd35cb5dafed55b85635783a4e
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-08-19 05:27:39 -0500

    Add files via upload

--- DateDifference/Program.cs
+++ DateDifference/Program.cs
@@ -6,7 +6,23 @@ namespace DateDifference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Title
+            Console.WriteLine("Days Between Dates");
+            Console.WriteLine("Enter first date: "); //Prompts user to enter date.
+            DateTime first = DateTime.Parse(Console.ReadLine()); //Makes what the user entered a date.
+            Console.WriteLine(first.ToShortDateString()); //Displays what the user entered as m/dd/yyyy.
+
+            Console.WriteLine("Enter second date: "); //Prompts user to enter date.
+            DateTime second = DateTime.Parse(Console.ReadLine()); //Makes what the user entered a date.
+            Console.WriteLine(second.ToShortDateString()); //Displays what the user entered as m/dd/yyyy.
+
+            TimeSpan result = first - second; //New timespan created to calculate time between the first and second date.
+
+            
+            //Prints how many days the timespan is.
+            Console.WriteLine("Days: " + result.Days);
+            Console.ReadLine();
+ 
         }
     }
 }
